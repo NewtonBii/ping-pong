d5657c9e2d9a41ee71c41ac2674706976e0f9fac
Newton Bii <biinewton382@gmail.com>
2017-10-27 21:47:54 +0300

    Add jscript for first behavior to alert ping.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $("form.ping").submit(function(event){
+  var number = parseInt($("input").val());
+  for (var index = 0; index <= number; index+=1) {
+  if (index%3==0) {
+    alert("ping")
+  } else {
+    alert(index)
+  }
+  }
+  event.preventDefault();
+});
+/*$("form.ping").submit(function(event){
   var answer=$("input").val();
   $("#output").text(answer);
   event.preventDefault();
 
-});
+});*/
