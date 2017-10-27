fa10af38123448e189ab1ba32584266a739417a4
Newton Bii <biinewton382@gmail.com>
2017-10-27 22:20:49 +0300

    Add elseif statement and if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ $("form.ping").submit(function(event){
   var number = parseInt($("input").val());
   for (var index = 1; index <= number; index+=1) {
   if (index%3==0) {
-    $("#output ul").append('<li>'+"ping"+'</li>')
-  } else {
+    $("#output ul").append('<li>'+"Ping"+'</li>')
+  }
+  else if (index%5==0) {
+    $("#output ul").append('<li>'+"Pong"+'</li>')
+  }
+  else if (index%15==0) {
+    $("#output ul").append('<li>'+"PingPong"+'</li>')
+  }
+  else {
     $("#output ul").append('<li>'+index+'</li>')
   }
+
   }
+
   event.preventDefault();
 });
-/*$("form.ping").submit(function(event){
-  var answer=$("input").val();
-  $("#output").text(answer);
-  event.preventDefault();
-
-});*/
