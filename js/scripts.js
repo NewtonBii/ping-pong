79f4bea5164bed9f15ff1d3470978a7494ca0450
Newton Bii <biinewton382@gmail.com>
2017-10-29 20:48:39 +0300

    Add files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-$(document).ready(function(event){
-  $("form#numbers").submit(function(){
-
-  var index = $("input#number-entered").val();
-  alert(index);
+$("form.ping").submit(function(event){
+  var number = parseInt($("input").val());
+  for (var index = 1; index <= number; index+=1) {
+  if (index%15==0) {
+    $("#output ul").append('<li>'+"PingPong"+'</li>')
+  }
+  else if (index%5==0) {
+    $("#output ul").append('<li>'+"Pong"+'</li>')
+  }
+  else if (index%3==0) {
+    $("#output ul").append('<li>'+"Ping"+'</li>')
+  }
+  else {
+    $("#output ul").append('<li>'+index+'</li>')
+  }
+  }
   event.preventDefault();
-  });
+
+
 });
