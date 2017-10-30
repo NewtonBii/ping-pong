26637e48ed3148877f81cc8eda9f353abb57558b
Newton Bii <biinewton382@gmail.com>
2017-10-30 10:18:28 +0300

    Merge js with gh-pages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $("form.ping").submit(function(event){
+  $("#output ul").empty();
   var number = parseInt($("input").val());
   for (var index = 1; index <= number; index+=1) {
   if (index%15==0) {
@@ -14,7 +15,7 @@ $("form.ping").submit(function(event){
     $("#output ul").append('<li>'+index+'</li>')
   }
   }
+  $("form.ping")[0].reset();
+
   event.preventDefault();
-
-
-});
+  });
